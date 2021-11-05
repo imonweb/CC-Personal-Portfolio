8eae0501c952a8a129f334fea3b188753f4c2a87
imonweb <contact@imonweb.co.uk>
2021-11-05 22:41:12 +0000

    completed

---
+++ src/js/app.js
@@ -0,0 +1,43 @@
+const navbar = document.querySelector(".navbar")
+const navbarOffsetTop = navbar.offsetTop
+const sections = document.querySelectorAll('section')
+const navbarLinks = document.querySelectorAll('.navbar-link')
+const progress = document.querySelector('.progress-bars-wrapper')
+const progressBarPercents = [97, 89, 85, 87, 80, 70, 50]
+
+window.addEventListener('scroll', (navbarOffsetTop) => {
+  mainFn()
+})
+
+const mainFn = () => {
+  // console.log(window.pageYOffset, navbar.offsetTop);
+  if(window.pageYOffset >= navbar.offsetTop){
+    // console.log("sticky");
+    navbar.classList.add('sticky')
+  } else {
+    navbar.classList.remove('sticky');
+  }
+
+  sections.forEach((section, i) => {
+    if(window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach(navbarLink => {
+        navbarLink.classList.remove('change')
+      })
+      navbarLinks[i].classList.add('change');
+    }
+  })
+
+  if(window.pageYOffset + window.innerHeight >= progress.offsetTop){
+    document.querySelectorAll('.progress-percent').forEach((el, i) => {
+      el.style.width = `${progressBarPercents[i]}%`
+      el.previousElementSibling.firstElementChild.textContent =progressBarPercents[i]
+       
+    })
+  }
+}
+
+mainFn()
+
+window.addEventListener('resize', () => {
+  window.location.reload();
+})
